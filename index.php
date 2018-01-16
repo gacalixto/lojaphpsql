e9742d727a9c5dc8d3f829fda83f777cd0c91c0f
Gabriel <gabrielcalixto9@gmail.com>
2018-01-16 19:11:23 -0200

    add login form

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php include("cabecalho.php"); ?>
             <h1>Bem vindo!</h1>
+
+            <h2>Login</h2>
+            <form action = "login.php">
+            <table>
+                <tr>
+                    <td>Email</td>
+                    <td><input class="form-control" type= "email" name = "email"></td>
+                </tr>
+                <tr>
+                    <td>Senha</td>
+                    <td><input class="form-control" type= "password" name = "senha"></td>
+                 </tr>
+                 <tr>
+                    <td><button class="btn btn-primary">Entrar</button></td>
+                 </tr>
+            </table>
+            </form>
+
 <?php include("rodape.php"); ?>
